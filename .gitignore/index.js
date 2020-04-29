2ee283015344b2de56c9860553757724a16d06c1
branizium <49403172+branizium@users.noreply.github.com>
2020-04-29 18:33:14 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,6 +8,29 @@ const warns = JSON.parse(fs.readFileSync('./warns.json'))
  
 client.login(process.env.TOKEN);
 
+
+
+module.exports.run = async (bot, message, args) => {
+   
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas la permission.")
+ 
+    let DMember = message.mentions.members.first() || message.guild.members.get(args[0])
+    if(!DMember) return message.channel.send("Veuillez mettre l'utilisateur pour envoyer le message")
+ 
+    let email = args.join(" ").slice(22);
+    if(!email) return message.channel.send("Vous devez mettre le messages pour l'envoie.")
+ 
+    let MSG = new Discord.RichEmbed()
+    .setDescription("VOUS AVEZ UN MAIL DU SERVEUR " + ` ${message.guild.name} ` + " | " + `${email}`)
+    .setColor(colours.white)
+ 
+    DMember.send(MSG)
+}
+ 
+module.exports.help = {
+    name: "dm"
+}
+
 client.on('guildMemberAdd', member =>{
     let embed = new Discord.RichEmbed()
         .setDescription(':tada: **' + member.user.username + '** a rejoint ' + member.guild.name)
